7832e26f530cdfcfaf3e90ae14eac7a558580878
saad-stuxnet <saadsheikh1491@gmail.com>
2026-03-28 22:55:51 +0530

    Update Vite config to use empty object for options

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({"/"}) => {
+export default defineConfig(({}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: "/"
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
